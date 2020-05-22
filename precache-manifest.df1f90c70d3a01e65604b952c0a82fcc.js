995ea606d53dd4c0d93e4c0f63edf888b3a04d13
mikkame <mikkame@users.noreply.github.com>
2020-05-22 14:00:26 +0000

    deploy: 6c04ee4547593cf0d5b3a0e6125fff3e126b3e17

---
+++ precache-manifest.df1f90c70d3a01e65604b952c0a82fcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c49027713d7150b62c",
+    "url": "/css/app.57415e9b.css"
+  },
+  {
+    "revision": "392de0bdf9b1b0e03d98",
+    "url": "/css/chunk-vendors.f0e8efde.css"
+  },
+  {
+    "revision": "5d3457e55f95f9a390152d93a55b6194",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96c49027713d7150b62c",
+    "url": "/js/app.6ee37950.js"
+  },
+  {
+    "revision": "392de0bdf9b1b0e03d98",
+    "url": "/js/chunk-vendors.46f6f8c1.js"
+  },
+  {
+    "revision": "93cec95f2621a081fb85c86db0850514",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
